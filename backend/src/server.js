8b378081dd32aa29024643146cfaeb520be11028
Harsh-Kumar-Pandit <harshkumarpandit2004@gmail.com>
2026-03-16 00:30:50 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,25 @@
-import express, { json } from "express"
+import express from "express"
+import path from "path";
 import { ENV } from "./lib/env.js";
 
 const app = express();
 const Port = ENV.PORT;
 const Mongo_Url = ENV.MONGO_URL;
 
+const __dirname = path.resolve();
+
 app.get("/health", (req, res)=> {
     res.status(200).json({msg:"api is up and running"});
 })
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(Port, () => {
     console.log("Server is running on port ", Port);
