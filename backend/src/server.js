7455e916940cfcbb3d230d226ba4f96210b92c83
Harsh-Kumar-Pandit <harshkumarpandit2004@gmail.com>
2026-03-16 18:27:10 +0530

    db and tool setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
-import express from "express"
+import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const Port = ENV.PORT;
@@ -8,20 +9,27 @@ const Mongo_Url = ENV.MONGO_URL;
 
 const __dirname = path.resolve();
 
-app.get("/health", (req, res)=> {
-    res.status(200).json({msg:"api is up and running"});
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(Port, () => {
-    console.log("Server is running on port ", Port);
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(Port, () => console.log("Server is running on port", Port));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
     
-})
+  }
+};
+
+startServer();
